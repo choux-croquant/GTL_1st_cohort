03e788a1a090df19c0fa3ff9125f5911f6c4dd6e
hgchoi11 <hgchoi11@pearlabyss.com>
2026-01-20 18:49:50 +0900

    remove unused default val

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Cloth/ClothSimulationData.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Cloth/ClothSimulationData.h
@@ -133,8 +133,7 @@ struct FClothSimulationData
 
     // External forces
     FVector Gravity = FVector(0.0f, 0.0f, 0.0f); // cm/s^2
-    // FVector Gravity = FVector(0.0f, 0.0f, 0.0f); // cm/s^2
-    FVector Wind = FVector(300.0f, 0.0f, 0.0f);
+    FVector Wind = FVector(0.0f, 0.0f, 0.0f);
     FVector ExternalForce = FVector(0.0f, 0.0f, 0.0f);
 
     // Timing
@@ -142,8 +141,7 @@ struct FClothSimulationData
     float AccumulatedTime = 0.0f;
 
     FClothSimulationData()
-        : NumParticles(0), NumConstraints(0), NumBendConstraints(0), Gravity(0.0f, 0.0f, 0.0f), Wind(300.0f, 0.0f, 0.0f), ExternalForce(0.0f, 0.0f, 0.0f), CurrentTime(0.0f), AccumulatedTime(0.0f)
-    //: NumParticles(0), NumConstraints(0), Gravity(0.0f, 0.0f, 0.0f), Wind(0.0f, 0.0f, 0.0f), ExternalForce(0.0f, 0.0f, 0.0f), CurrentTime(0.0f), AccumulatedTime(0.0f)
+        : NumParticles(0), NumConstraints(0), NumBendConstraints(0), Gravity(0.0f, 0.0f, 0.0f), Wind(0.0f, 0.0f, 0.0f), ExternalForce(0.0f, 0.0f, 0.0f), CurrentTime(0.0f), AccumulatedTime(0.0f)
     {
     }
 };
